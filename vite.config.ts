46a56ad8f6760c24858c047a6156172ea00ff985
jdw5 <joshuadwallace1@gmail.com>
2024-11-25 21:35:25 +1000

    Fixed issue with type resolution

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,31 @@
 import { resolve } from "path"
 import { defineConfig } from "vite"
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
+  plugins: [dts({
+      exclude: ["src/test/**"],
+      rollupTypes: true,
+      insertTypesEntry: true,
+      include: ["src/**/*.ts", "src/**/*.d.ts", "src/**/*.tsx", "src/**/*.vue"],
+      outDir: "dist",
+    }),
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
-      name: ":package_title",
-      fileName: `:package_name`,
+      name: 'query',
+      fileName: 'index',
     },
     rollupOptions: {
-      external: [],
+      external: ['vue', '@inertiajs/vue3', 'axios'],
+      output: {
+        globals: {
+          vue: 'Vue',
+          '@inertiajs/vue3': 'Inertia',
+          axios: 'axios',
+        },
+      },
     },
   },
 })
